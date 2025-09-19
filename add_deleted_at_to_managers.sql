bf65bfada5cb68a6eff50db6aa6068c2be124998
Roy Nabesis <roynabesis.main@gmail.com>
2025-09-20 00:20:46 +0800

    Updating...

--- add_deleted_at_to_managers.sql
+++
@@ -1,5 +0,0 @@
--- Add deleted_at column to managers table for soft delete functionality
-ALTER TABLE managers ADD COLUMN IF NOT EXISTS deleted_at DATETIME DEFAULT NULL;
-
--- Add index for better performance when querying deleted records
-CREATE INDEX IF NOT EXISTS idx_managers_deleted_at ON managers(deleted_at);
